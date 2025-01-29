b922374352a714aae8b33ba1e404f6b474d23fdf
Sudeep4686 <sudeephipparge@gmail.com>
2025-01-29 14:22:15 +0530

    created API to get user with id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const users=require('./MOCK_DATA.json');
 const fs=require('fs');
+const { join } = require('path');
 
 app.use(express.urlencoded({extended:false}));
 
@@ -12,7 +13,15 @@ app.get('/api/users',(req,res)=>{
 })
 
 //Get user with id
-
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    if(user){
+        return res.status(200).json({message:"User fetched successfully",user:user});
+    }else{
+        return res.status(404).json({message:"User not found"});
+    }
+})
 
 //POST user
 
