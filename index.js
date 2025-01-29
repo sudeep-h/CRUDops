00e29ff08649b0737e4581fab12f550c4b5a5aea
Sudeep4686 <sudeephipparge@gmail.com>
2025-01-29 14:45:40 +0530

    delete user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const users=require('./MOCK_DATA.json');
 const fs=require('fs');
 const { join } = require('path');
+const { json } = require('stream/consumers');
 
 app.use(express.urlencoded({extended:false}));
 
@@ -54,7 +55,18 @@ app.patch('/api/users/:id',(req,res)=>{
 })
 
 //delete user
-
+app.delete('/api/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const user=users.find((user)=>user.id==id);
+    const updatedUsers=users.filter((user)=>user.id!=id);
+    if(!user){
+        return res.status(404).json({message:"User not found"});
+    }else{
+        fs.writeFile('./MOCK_DATA.json',JSON.stringify(updatedUsers),(err)=>{
+            return res.status(200).json({message:"User deleted successfully"});
+        })
+    }
+})
 
 const PORT=8000;
 app.listen(PORT,()=>{
